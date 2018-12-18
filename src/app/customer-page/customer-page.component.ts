33d660074326e9d2006b082be8b9a84cc46c9b4e
Zack Blaylock <zackblaylock@gmail.com>
2018-12-18 14:58:20 -0500

    Added logout function and button to all pages

--- src/app/customer-page/customer-page.component.ts
+++ src/app/customer-page/customer-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'customer-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private $router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  handleLogout() {
+    sessionStorage.removeItem('user');
+    return this.$router.navigate(['cover']);
+  }
 }
--- src/app/inspect-officer-page/inspect-officer-page.component.ts
+++ src/app/inspect-officer-page/inspect-officer-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Report } from '../model/Report';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'inspect-officer-page',
@@ -7,31 +8,38 @@ import { Report } from '../model/Report';
   styleUrls: ['./inspect-officer-page.component.css']
 })
 export class InspectOfficerPageComponent implements OnInit {
-  reports:Report[];
-  reportTable:HTMLElement;
-  reportForm:HTMLElement;
-  claimId:number;
+  reports: Report[];
+  reportTable: HTMLElement;
+  reportForm: HTMLElement;
+  claimId: number;
 
-  //need service
-  constructor() { }
+  // need service
+  constructor(
+    private $router: Router
+    ) { }
+
 
   ngOnInit() {
-    //this.service.getReports().subscribe(data=>{this.reports=data;});
+    // this.service.getReports().subscribe(data=>{this.reports=data;});
   }
 
-  showForm(claimId){
-    this.claimId=claimId; //for use in submitReport()
-    this.reportTable=document.getElementById('reportTable') as HTMLElement;
-    this.reportTable.style.display='none';
-    this.reportForm=document.getElementById('reportForm') as HTMLElement;
-    this.reportForm.style.display='block';
+  handleLogout() {
+    sessionStorage.removeItem('user');
+    return this.$router.navigate(['cover']);
   }
-  submitReport(){
-    //send report to database
 
-    this.reportTable=document.getElementById('reportTable') as HTMLElement;
-    this.reportTable.style.display='block';
-    this.reportForm=document.getElementById('reportForm') as HTMLElement;
-    this.reportForm.style.display='none';
+  showForm(claimId) {
+    this.claimId = claimId; // for use in submitReport()
+    this.reportTable = document.getElementById('reportTable') as HTMLElement;
+    this.reportTable.style.display = 'none';
+    this.reportForm = document.getElementById('reportForm') as HTMLElement;
+    this.reportForm.style.display = 'block';
+  }
+  submitReport() {
+    // send report to database
+    this.reportTable = document.getElementById('reportTable') as HTMLElement;
+    this.reportTable.style.display = 'block';
+    this.reportForm = document.getElementById('reportForm') as HTMLElement;
+    this.reportForm.style.display = 'none';
   }
 }
