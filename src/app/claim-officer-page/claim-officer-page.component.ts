d1ffb762059a7cc8a910632ab199f3401c51e1fa
Zack Blaylock <zackblaylock@gmail.com>
2018-12-18 16:22:25 -0500

    Changed claim officer page from table format to cards.

--- src/app/claim-officer-page/claim-officer-page.component.ts
+++ src/app/claim-officer-page/claim-officer-page.component.ts
@@ -13,22 +13,22 @@ import {Router, RouterModule} from '@angular/router';
   styleUrls: ['./claim-officer-page.component.css']
 })
 export class ClaimOfficerPageComponent implements OnInit {
-  claims:Claim[];
-  c:Claim=new Claim();
-  users:any;
-  u:User=new User();
-  claimTable:HTMLElement;
-  inspectorTable:HTMLElement;
-  claim:Claim;
+  claims: Claim[];
+  c: Claim = new Claim();
+  users: any;
+  u: User = new User();
+  claimTable: HTMLElement;
+  inspectorTable: HTMLElement;
+  claim: Claim;
 
   constructor(
     private claimService: ClaimService,
-    private userService: UserService
-    private $router: Router,
+    private userService: UserService,
+    private $router: Router
     ) { }
 
   ngOnInit() {
-    this.claimService.getClaims().subscribe(data=>{this.claims=data;});
+    this.claimService.getClaims().subscribe(data => {this.claims = data; });
     this.userService.getUsersByType('inspect_officers')
           .subscribe(data => {
             this.users = data;
@@ -41,39 +41,39 @@ export class ClaimOfficerPageComponent implements OnInit {
     return this.$router.navigate(['cover']);
   }
 
-  showInspectors(claim){
-    this.claim=claim; //stored to be used in assignInspector
+  showInspectors(claim) {
+    this.claim = claim; // stored to be used in assignInspector
 
-    this.claimTable=document.getElementById('claimTable') as HTMLElement;
-    this.claimTable.style.display='none';
-    this.inspectorTable=document.getElementById('inspectorTable') as HTMLElement;
-    this.inspectorTable.style.display='block';
+    this.claimTable = document.getElementById('claimTable') as HTMLElement;
+    this.claimTable.style.display = 'none';
+    this.inspectorTable = document.getElementById('inspectorTable') as HTMLElement;
+    this.inspectorTable.style.display = 'block';
   }
-  assignInspector(lname){
-    //issue below. inspectOfficer is in the report table. Need to discuss how to handle.
-    //this.claim.inspectOfficer=lname;
-    
-    //TODO send to database. Not sure this works
-    //this.claimService.updateClaimByInspector(this.claim,this.claim.claimId);
-    this.claimTable=document.getElementById('claimTable') as HTMLElement;
-    this.claimTable.style.display='block';
-    this.inspectorTable=document.getElementById('inspectorTable') as HTMLElement;
-    this.inspectorTable.style.display='none';
+  assignInspector(lname) {
+    // issue below. inspectOfficer is in the report table. Need to discuss how to handle.
+    // this.claim.inspectOfficer=lname;
+
+    // TODO send to database. Not sure this works
+    // this.claimService.updateClaimByInspector(this.claim,this.claim.claimId);
+    this.claimTable = document.getElementById('claimTable') as HTMLElement;
+    this.claimTable.style.display = 'block';
+    this.inspectorTable = document.getElementById('inspectorTable') as HTMLElement;
+    this.inspectorTable.style.display = 'none';
   }
-  approve(claim){
-    this.claim=claim;
-    this.claim.status="Approved";
-    this.claimService.updateClaim(claim,this.claim.claimId);
+  approve(claim) {
+    this.claim = claim;
+    this.claim.status = 'Approved';
+    this.claimService.updateClaim(claim, this.claim.claimId);
   }
-  decline(claim){
-    this.claim=claim;
-    this.claim.status="Declined";
-    this.claimService.updateClaim(claim,this.claim.claimId);
+  decline(claim) {
+    this.claim = claim;
+    this.claim.status = 'Declined';
+    this.claimService.updateClaim(claim, this.claim.claimId);
   }
-  hider(){
-    this.claimTable=document.getElementById('claimTable') as HTMLElement;
-    this.claimTable.style.display='block';
-    this.inspectorTable=document.getElementById('inspectorTable') as HTMLElement;
-    this.inspectorTable.style.display='none';
+  hider() {
+    this.claimTable = document.getElementById('claimTable') as HTMLElement;
+    this.claimTable.style.display = 'block';
+    this.inspectorTable = document.getElementById('inspectorTable') as HTMLElement;
+    this.inspectorTable.style.display = 'none';
   }
 }
