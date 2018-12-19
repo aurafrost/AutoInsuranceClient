7d6dfebc19e7913e46f2d1cb5527cbab5fc5a214
Zack Blaylock <zackblaylock@gmail.com>
2018-12-19 15:23:04 -0500

    Report Service setup

--- src/app/model/Claim.ts
+++ src/app/model/Claim.ts
@@ -1,12 +1,12 @@
-import { User } from "./User";
-export class Claim{
-    claimId:number;
-    user:User;
-    status:String;
-    otherDriverLicense:String;
-    carModel:String;
-    carMake:String;
-    dateOfIncident:String;
-    description:String;
-    estimateCostRepairs:number;
+import { User } from './User';
+export class Claim {
+    claimId: number;
+    user: User;
+    status: String;
+    otherDriverLicense: String;
+    carModel: String;
+    carMake: String;
+    dateOfIncident: String;
+    description: String;
+    estimateCostRepairs: number;
 }
--- src/app/service/report/report.service.ts
+++ src/app/service/report/report.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Claim } from '../../model/Claim';
 import {BASE_URL} from '../../../environments/environment';
+import {Report} from '../../model/Report';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +12,25 @@ export class ReportService {
   constructor(private http: HttpClient) { }
 
   public getReports() {
-    return this.http.get<Claim[]>(`${BASE_URL}/reports`);
+    return this.http.get<Report[]>(`${BASE_URL}/reports`);
   }
   // getReportByID() {}
 
-  public postReport(report){
+  public postReport(report) {
     const headers = new HttpHeaders();
     headers.append('Access-Control-Allow-Origin', '*');
-    return this.http.post(`${BASE_URL}/reports`, report, {headers: headers});
+    return this.http.post<Report>(`${BASE_URL}/reports`, report, {headers: headers});
+  }
+
+  public getReportById(id) {
+    return this.http.get<Report>(`${BASE_URL}/reports/${id}`);
+  }
+
+  public deleteReportById(id) {
+    return this.http.delete<Report>(`${BASE_URL}/reports/${id}`);
+  }
+
+  public updateReportById(id, report) {
+    return this.http.put<Report>(`${BASE_URL}/reports/${id}`, report);
   }
 }
